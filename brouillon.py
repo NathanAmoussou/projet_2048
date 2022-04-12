681fef33d610038b313036d6e8e4bcc39ae20919
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2022-04-12 12:02:10 +0200

    Déplacement du code test pour le déplacement de la tuile vers nouveau fichier brouillon.py

---
+++ brouillon.py
@@ -0,0 +1,36 @@
+"""def deplacement_right_test():
+    global test_x
+    if test_x < 375:
+        canevas.move(tuile_test, 125, 0)
+        test_x += 125
+def deplacement_left_test():
+    global test_x
+    if test_x > 0:
+        canevas.move(tuile_test, -125, 0)
+        test_x += -125
+def deplacement_up_test():
+    global test_y
+    if test_y > 125:
+        canevas.move(tuile_test, 0, -125)
+        test_y += -125
+def deplacement_down_test():
+    global test_y
+    if test_y < 500:
+        canevas.move(tuile_test, 0, 125)
+        test_y += 125"""
+
+"""bouton_left = tk.Button(racine, text='Left', bg=rgb_hack((37, 37, 37)), command=deplacement_left_test)
+bouton_right = tk.Button(racine, text='Right', bg=rgb_hack((37, 37, 37)), command=deplacement_right_test)
+bouton_up = tk.Button(racine, text='Up', bg=rgb_hack((37, 37, 37)), command=deplacement_up_test)
+bouton_down = tk.Button(racine, text='Down', bg=rgb_hack((37, 37, 37)), command=deplacement_down_test)
+tuile_test = canevas.create_rectangle(125, 125, 250, 250, fill=rgb_hack((0, 0, 0)))"""
+
+"""bouton_left.place(x=700, y=300) # boutons très moches juste pour tester
+bouton_right.place(x=700, y=350) # boutons très moches juste pour tester
+bouton_up.place(x=700, y=325) # boutons très moches juste pour tester
+bouton_down.place(x=700, y=375) # boutons très moches juste pour tester
+
+
+test_c = canevas.coords(tuile_test) # temporaire, c'est juste pour les tests
+test_x = test_c[1]
+test_y = test_c[2]"""
