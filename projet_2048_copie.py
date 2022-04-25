1bff5c02b5379900d0ae0d9f9be807c9bc420735
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2022-04-25 19:25:40 +0200

    ajout dans le brouillon de la version avec le text indiquant le numéro de la tuile, qui se déplace en même temps que la tuile

--- projet_2048_copie.py
+++ projet_2048_copie.py
@@ -1,4 +1,3 @@
-###################################
 # Import des librairies
 
 import tkinter as tk
@@ -22,7 +21,7 @@ LONGUEUR_TUILE = 125
 # Définition des fonctions
 
 def rgb_hack(rgb):
-    """Fonction qui permet de travailler avec des couleurs RGB (https://pythonguides.com/python-tkinter-colors/)."""
+    # Fonction qui permet de travailler avec des couleurs RGB (https://pythonguides.com/python-tkinter-colors/).
     return "#%02x%02x%02x" % rgb
 
 
@@ -31,21 +30,25 @@ def deplacement_right_test():
     if test_x < 375:
         canevas.move(tuile_test, 125, 0)
         test_x += 125
+        canevas.move(tuile_test_text, 125, 0)
 def deplacement_left_test():
     global test_x
     if test_x > 0:
         canevas.move(tuile_test, -125, 0)
         test_x += -125
+        canevas.move(tuile_test_text, -125, 0)
 def deplacement_up_test():
     global test_y
     if test_y > 125:
         canevas.move(tuile_test, 0, -125)
         test_y += -125
+        canevas.move(tuile_test_text, 0, -125)
 def deplacement_down_test():
     global test_y
     if test_y < 500:
         canevas.move(tuile_test, 0, 125)
         test_y += 125
+        canevas.move(tuile_test_text, 0, 125)
 
 
 ###################################
@@ -64,7 +67,8 @@ bouton_left = tk.Button(racine, text='Left', bg=rgb_hack((37, 37, 37)), command=
 bouton_right = tk.Button(racine, text='Right', bg=rgb_hack((37, 37, 37)), command=deplacement_right_test)
 bouton_up = tk.Button(racine, text='Up', bg=rgb_hack((37, 37, 37)), command=deplacement_up_test)
 bouton_down = tk.Button(racine, text='Down', bg=rgb_hack((37, 37, 37)), command=deplacement_down_test)
-tuile_test = canevas.create_rectangle(125, 125, 250, 250, fill=rgb_hack((0, 0, 0)))
+
+tuile_test = canevas.create_rectangle(125, 125, 250, 250, fill=rgb_hack((238, 228, 218)))
 
 ## placement des widgets
 canevas.place(x=50, y=50)
@@ -81,43 +85,8 @@ test_c = canevas.coords(tuile_test) # temporaire, c'est juste pour les tests
 test_x = test_c[1]
 test_y = test_c[2]
 
+tuile_test_text = canevas.create_text(test_x+125//2, test_y-125//2, text="2", fill="black")
+print(test_c)
+
 ## boucle principale
 tk.mainloop()
-
-# CODE RELATIF AU DÉPLACEMENT D'UNE TUILE
-"""def deplacement_right_test():
-    global test_x
-    if test_x < 375:
-        canevas.move(tuile_test, 125, 0)
-        test_x += 125
-def deplacement_left_test():
-    global test_x
-    if test_x > 0:
-        canevas.move(tuile_test, -125, 0)
-        test_x += -125
-def deplacement_up_test():
-    global test_y
-    if test_y > 125:
-        canevas.move(tuile_test, 0, -125)
-        test_y += -125
-def deplacement_down_test():
-    global test_y
-    if test_y < 500:
-        canevas.move(tuile_test, 0, 125)
-        test_y += 125"""
-
-"""bouton_left = tk.Button(racine, text='Left', bg=rgb_hack((37, 37, 37)), command=deplacement_left_test)
-bouton_right = tk.Button(racine, text='Right', bg=rgb_hack((37, 37, 37)), command=deplacement_right_test)
-bouton_up = tk.Button(racine, text='Up', bg=rgb_hack((37, 37, 37)), command=deplacement_up_test)
-bouton_down = tk.Button(racine, text='Down', bg=rgb_hack((37, 37, 37)), command=deplacement_down_test)
-tuile_test = canevas.create_rectangle(125, 125, 250, 250, fill=rgb_hack((0, 0, 0)))"""
-
-"""bouton_left.place(x=700, y=300) # boutons très moches juste pour tester
-bouton_right.place(x=700, y=350) # boutons très moches juste pour tester
-bouton_up.place(x=700, y=325) # boutons très moches juste pour tester
-bouton_down.place(x=700, y=375) # boutons très moches juste pour tester
-
-
-test_c = canevas.coords(tuile_test) # temporaire, c'est juste pour les tests
-test_x = test_c[1]
-test_y = test_c[2]"""
